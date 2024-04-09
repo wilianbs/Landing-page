0a92eb493d5114132d460a31f3361d2c830aff49
Wilian Batista <wilian.batista.santos@gmail.com>
2024-04-09 16:56:17 -0300

    Layout

--- landing-page/src/pages/components/Footer.js
+++ landing-page/src/pages/components/Footer.js
@@ -1,10 +1,26 @@
+import styles from '../../styles/Footer.module.css';
+import Image from 'next/image'; 
+
 export default function Footer() {
-    return(
-        <footer>
+    return (
+        <footer className={styles.footer}>
+            <div>
+                <Image
+                    src="/images/logo.jpg"
+                    width="50"
+                    height="50"
+                    alt="Logo Suellen Louzada"
+                />
+            </div>
             <p>
-                <samp>Suellen Louzada</samp>
+                <span>Suellen Louzada </span>
                 &copy; 2024
             </p>
+            <div className={styles.midia}>
+                <p>Instagran</p>
+                <p>WhatsApp</p>
+                <p>Facebook</p>
+            </div>
         </footer>
-    )
+    );
 }
--- landing-page/src/pages/components/Navbar.js
+++ landing-page/src/pages/components/Navbar.js
@@ -1,28 +1,39 @@
 import Link from "next/link"
 import Image from "next/image"
 
+import styles from '../../styles/Navbar.module.css'
 
 export default function Navbar() {
     return(
-        <nav>
-            <div>
-                <Image
-                    src="/image/vercel.svg"
-                    width="30"
-                    height="30"
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Image className={styles.image_logo}
+                    src="/images/logo.jpg"
+                    width="100"
+                    height="100"
                     alt="Logo Suellen Louzada"                
                 />
                 <h1>Suellen Louzada</h1>
             </div>
-            <ul>
+            <ul className={styles.link_items}>
                 <li>
                     <Link href="/">
                         Home
                     </Link>
                 </li>
                 <li>
+                    <Link href="/produtos">
+                        Produtos
+                    </Link>
+                </li>
+                <li>
                     <Link href="/about">
                         Sobre
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contato">
+                        Contato
                     </Link>
                 </li>
             </ul>
