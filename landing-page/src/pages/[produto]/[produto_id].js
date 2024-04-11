f240591a47c386e874a43bce02b5bfc386c59077
Wilian Batista <wilian.batista.santos@gmail.com>
2024-04-11 18:02:42 -0300

    conteudo dinamico

--- landing-page/src/pages/[produto]/[produto_id].js
+++ landing-page/src/pages/[produto]/[produto_id].js
@@ -1,6 +1,97 @@
-export default function Produto({produto}) {
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import style from '../../styles/Produto_id.module.css'
+import Image from 'next/image';
+
+export default function Produto() {
+  const router = useRouter();
+  const { produto_id } = router.query;
+  const [produto, setProduto] = useState(null);
+
+  const [indiceImagem, setIndiceImagem] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Faz a requisição HTTP para obter os dados do arquivo JSON
+        const response = await fetch('/data.json');
+        if (!response.ok) {
+          throw new Error('Erro ao carregar os dados do produto');
+        }
+        const data = await response.json();
+        // Encontra o produto correspondente ao ID na lista de produtos
+        const foundProduto = data.produtos.find((p) => p.id === parseInt(produto_id));
+        setProduto(foundProduto);
+      } catch (error) {
+        console.error('Erro:', error.message);
+      }
+    };
+
+    // Verifica se o ID do produto está definido e faz a requisição dos dados
+    if (produto_id) {
+      fetchData();
+    }
+  }, [produto_id]);
+
+  if (!produto) {
+    return <p>Produto não encontrado</p>;
+  }
+
+  const proximaImagem = () => {
+    if (indiceImagem < produto.imagens.length - 1) {
+      setIndiceImagem(indiceImagem + 1);
+    }
+  };
+
+  // Função para voltar para a imagem anterior
+  const imagemAnterior = () => {
+    if (indiceImagem > 0) {
+      setIndiceImagem(indiceImagem - 1);
+    }
+  };
+  const WhatsApp = () => {
     return(
-        <p>teste</p>
-    
+        <a href="https://wa.me/5512991762552"></a>
     )
+  };
+
+  return (
+    <div className={style.container} >
+
+        
+        <div className={style.descricao}>
+            <h1>{produto.nome}</h1>
+            <span>{produto.valor}</span>
+            <p>{produto.descricao}</p>
+            
+                    <a href={`https://wa.me/5512991762552?text=Olá, gostaria de saber mais sobre a ${produto.nome}`}>
+                        Comprar pelo WhatsApp
+                    </a>
+            
+            
+        </div>
+        <div className={style.container_image}>
+            <div className={style.image}>
+                <Image
+                src={produto.imagens[indiceImagem]}
+                width={400}
+                height={400}
+                alt={produto.nome} 
+                />
+                
+            </div>
+            <div className={style.btn}>
+                    <button onClick={imagemAnterior}>Voltar</button>
+                    <button onClick={proximaImagem}>Próximo</button>
+            </div>
+            
+        </div>
+        
+        <div className={style.background} style={{ backgroundImage: `url(${produto.fotos})`}}  >
+
+        </div>
+      
+    </div>
+  );
 }
--- landing-page/src/pages/index.js
+++ landing-page/src/pages/index.js
@@ -1,27 +1,21 @@
-
+// pages/index.js
 import styles from '../styles/Home.module.css';
 import Card from '../components/Card';
-
-const produtos=[
-  {id:0, nome:'Bolsa de campo', valor:'R$ 35,90', resumo:'Resumo do produto', descricao:'Texto do produto', fotos: ['123', '456','789' ] },
-  {id:1, nome:'Bolsa Masculina', valor:'R$ 45,90', resumo:'Resumo do produto', descricao:'Texto do produto', fotos: ['123', '456','789' ] },
-  {id:2, nome:'Pasta de Campo', valor:'R$ 25,90', resumo:'Resumo do produto', descricao:'Texto do produto', fotos: ['123', '456','789' ] },
-  {id:3, nome:'Pastinha', valor:'R$ 15,90', resumo:'Resumo do produto', descricao:'Texto do produto', fotos: ['123', '456','789' ] },
-  {id:4, nome:'Pastinha trasparente', valor:'R$ 18,90', resumo:'Resumo do produto', descricao:'Texto do produto', fotos: ['123', '456','789' ] }
-]
+import data from '../../public/data.json';
 
 export default function Home() {
+  const produtos = data.produtos;
+
   return (
     <>
-      <div className={styles.title_container }>
+      <div className={styles.title_container}>
         <h1>Bestsellers</h1>
       </div>
-      
+
       <div className={styles.produtos_container}>
         {produtos.map((produto) => (
-          <Card key={produto.id} produto={produto}/> 
+          <Card key={produto.id} produto={produto} />
         ))}
-        
       </div>
     </>
   );
